48a22be72c7c1cf00df639b2da054dcf66ed6ab8
nickramsay19 <nickramsay19@gmail.com>
2023-05-15 23:37:16 +1000

    frontend, db connection

--- app/app.py
+++ app/app.py
@@ -1,10 +1,37 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+from .database import db, Post
+
+# setup database connection
+'''with db:
+    db.create_tables([Post])
+
+    # create dummy post
+    dummy_post = Post(title="dummy title", body="dummy body")
+    dummy_post.save()'''
+
+# setup main router app
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    #return {"message": "Hello World"}
-    return "well hello there!"
+# setup templates and static files
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def post(request: Request):
+    return templates.TemplateResponse("index.html", {
+                "request": request,
+    }) 
+
+@app.get("/{post_id}", response_class=HTMLResponse)
+async def post(request: Request, post_id: str):
+    return templates.TemplateResponse("post.html", {
+                "request": request,
+                "post_id": post_id,
+    }) 
+    
 
 
